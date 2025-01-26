880b0b5dc9495656c53e22e3653496dc116af44f
cjwattttts <cameron.jwatts@outlook.com>
2025-01-26 18:53:50 -0500

    Task 1 - Employee Information.

--- cc_1.js
+++ cc_1.js
@@ -1,1 +1,12 @@
 Initial commit - JavaScript Basics Challenge Setup.
+
+let employeeName = "Cam Watts";
+const employeeID = 10101;
+var isActive = true;
+
+console.log("Employee Name: " + employeeName);
+console.log("Employee ID: " + employeeID);
+console.log("Is Active: " + isActive);
+console.log("Type of Employee Name: " + typeof employeeName);
+console.log("Type of Employee ID: " + typeof employeeID);
+console.log("Type of Is Active: " + typeof isActive);
