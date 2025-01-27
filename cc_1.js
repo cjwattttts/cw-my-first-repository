2e09c9a05dca50e329a067e90234f118a2e3b0d5
cjwattttts <cameron.jwatts@outlook.com>
2025-01-26 19:22:16 -0500

    Task 2 - Product Details.

--- cc_1.js
+++ cc_1.js
@@ -1,5 +1,3 @@
-Initial commit - JavaScript Basics Challenge Setup.
-
 let employeeName = "Cam Watts";
 const employeeID = 10101
 var isActive = true;
@@ -7,3 +5,12 @@ var isActive = true;
 console.log("Cam Watts", employeeName, "| Type;", typeof employeeName);
 console.log("10101", employeeID, "| Type;", typeof employeeID);
 console.log("true", isActive, "| Type;", typeof isActive);
+
+let productName = "Phone Case";
+const productPrice = 79.99; 
+var isAvailable = true;
+
+console.log ("Product Name:", productName, "| Type:", typeof productName);
+console.log ("Product Price:", productPrice, "| Type:", typeof productPrice);
+console.log ("Is Available:", isAvailable, "| Type:", typeof isAvailable);
+
