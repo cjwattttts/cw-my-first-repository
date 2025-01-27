95545458412f63de57aa79a34f59af755486f5c1
cjwattttts <cameron.jwatts@outlook.com>
2025-01-26 20:44:24 -0500

    Task 2 - Student Scores.

--- cc_2.js
+++ cc_2.js
@@ -1,3 +1,13 @@
 let products = ["Phone Case", "Charger", "Computer", "Headphones", "Screen Protector"];
 products.push("Adaptor");
 console.log("Updated Product List", products);
+
+let scores = [80, 78, 89, 69, 95]; 
+scores[1]=95 
+let total = 0;
+for (let 1 = 0; i<scores.length; i++) {
+    total += scores[i];
+}
+let average = total / scores.length;
+console.log("Updated Scores:", scores);
+console.log("Average Scores:", average.toFixed(2));
