7c97b8db9ba9be3a57192768d12d0da2b3b4e88f
cjwattttts <cameron.jwatts@outlook.com>
2025-01-26 21:07:26 -0500

    Error Fixing

--- cc_2.js
+++ cc_2.js
@@ -3,9 +3,9 @@ products.push("Adaptor");
 console.log("Updated Product List", products);
 
 let scores = [80, 78, 89, 69, 95]; 
-scores[1]=95 
+scores[1]=95;
 let total = 0;
-for (let 1 = 0; i<scores.length; i++) {
+for (let i = 0; i<scores.length; i++) {
     total += scores[i];
 }
 let average = total / scores.length;
@@ -20,11 +20,11 @@ employee.position = "Assistant Manager";
 console.log("Updated Employee Position:", employee);
 
 let customers = [
-    {name: "Sarah Jane" email: "sarahjane@gmail.com", purchaseAmount: 200},
-    {name: "Ethan Marks" email: "marks10@gmail.com", purchaseAmount: 367},
-    {name: "Emily Jacobs" email: "emmyj2@gmail.com", purchaseAmount: 850},
+    {name: "Sarah Jane", email: "sarahjane@gmail.com", purchaseAmount: 200},
+    {name: "Ethan Marks", email: "marks10@gmail.com", purchaseAmount: 367},
+    {name: "Emily Jacobs", email: "emmyj2@gmail.com", purchaseAmount: 850},
 ];
-let newCustomers = {name: "Jimmy Bonds," email: "Jbond34@yahoo.com", purchaseAmount 260};
+let newCustomers = {name: "Jimmy Bonds", email: "Jbond34@yahoo.com", purchaseAmount: 260};
 console.log("Customer database:", customers);
 
 let order = {
