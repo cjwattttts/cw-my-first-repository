8855054c3f505b7f27d780a1269911eea01dc831
cjwattttts <cameron.jwatts@outlook.com>
2025-02-02 15:50:32 -0500

    Task 2 - Modifying Customer Orders

--- cc_3.js
+++ cc_3.js
@@ -4,3 +4,10 @@ prices.push(34.99); // adds new price to array
 prices.shift(); // removes first price from array
 console.log('Price List Updated') // updates price list in console
 
+//TASK 2
+let orders = [5, 10, 15, 20, 15] // declares an array called orders
+orders [2] += 5; // adds 5 to order 3
+let totalOrders = orders.reduce((sum,num) => sum + num, 0); // calculates the total number of all orders
+console.log('Orders Updated', orders); // updates order list in console
+console.log('Order Count Total', totalOrders); // updates order count total in console
+
