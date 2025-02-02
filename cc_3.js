67d36eeb9ab02bde0e505bdb74305509c28e5b10
cjwattttts <cameron.jwatts@outlook.com>
2025-02-02 16:26:49 -0500

    Format Adjustment

--- cc_3.js
+++ cc_3.js
@@ -1,17 +1,17 @@
-//TASK 1
+// TASK 1
 let prices = [9.99, 14.99, 19.99, 24.99, 29.99] // declares array called prices
 prices.push(34.99); // adds new price to array
 prices.shift(); // removes first price from array
 console.log('Price List Updated') // updates price list in console
 
-//TASK 2
+// TASK 2
 let orders = [5, 10, 15, 20, 15] // declares an array called orders
 orders [2] += 5; // adds 5 to order 3
 let totalOrders = orders.reduce((sum,num) => sum + num, 0); // calculates the total number of all orders
 console.log('Orders Updated', orders); // updates order list in console
 console.log('Order Count Total', totalOrders); // updates order count total in console
 
-//TASK 3
+// TASK 3
 let employee = {
     name: 'Jack Shepard', // sets name
     role: 'Business Analyst', // sets position
@@ -22,7 +22,7 @@ employee.performaceScore = 90; // perfomance score fell
 employee.promotionEligible = employee.performanceScore >= 90 // assesing employee eligibility
 console.log('Employee Updated', employee); // logs updated employee in console
 
-//TASK 4
+// TASK 4
 let feedback = [
     { customerName: 'Hugo', feedbackText: 'Very quick and helpful.', rating: 10 }, // declares Hugo's rating
     { customerName: 'Kate', feedbackText: 'Extremely kind!', rating: 9 } // declares Kate's rating
@@ -31,7 +31,7 @@ let feedback = [
 feedback.push({ customerName: 'Claire', feedbackText: 'Brilliant and super helpful!', rating: 8}); // adds Claire's feedback to array
 console.log('Customer Feedback:', feedback); // logs customer feedback in console
 
-//TASK 5
+// TASK 5
 let inventory = {
     itemName: 'Transmitter', // sets item name
     stockCount: '350', // sets stock count
