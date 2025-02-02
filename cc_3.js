4d7cd7fca0a49b8c1d5ab160a29408e242941e31
cjwattttts <cameron.jwatts@outlook.com>
2025-02-02 17:33:19 -0500

    Final Touches + Repairs

--- cc_3.js
+++ cc_3.js
@@ -1,12 +1,12 @@
 // TASK 1
-let prices = [9.99, 14.99, 19.99, 24.99, 29.99] // declares array called prices
+let prices = [9.99, 14.99, 19.99, 24.99, 29.99]; // declares array called prices
 prices.push(34.99); // adds new price to array
 prices.shift(); // removes first price from array
-console.log('Price List Updated') // updates price list in console
+console.log('Price List Updated'); // updates price list in console
 
 // TASK 2
-let orders = [5, 10, 15, 20, 15] // declares an array called orders
-orders [2] += 5; // adds 5 to order 3
+let orders = [5, 10, 15, 20, 15]; // declares an array called orders
+orders[2] += 5; // adds 5 to order 3
 let totalOrders = orders.reduce((sum,num) => sum + num, 0); // calculates the total number of all orders
 console.log('Orders Updated', orders); // updates order list in console
 console.log('Order Count Total', totalOrders); // updates order count total in console
@@ -19,7 +19,7 @@ let employee = {
     isActive: true // employee is currently working in company 
 };
 employee.performanceScore = 90; // perfomance score fell
-employee.promotionEligible = employee.performanceScore >= 90 // assesing employee eligibility
+employee.promotionEligible = employee.performanceScore >= 90; // assesing employee eligibility
 console.log('Employee Updated', employee); // logs updated employee in console
 
 // TASK 4
@@ -35,10 +35,10 @@ console.log('Customer Feedback:', feedback); // logs customer feedback in consol
 let inventory = {
     itemName: 'Transmitter', // sets item name
     stockCount: 350, // sets stock count
-    price: 200 // sets item price
-};
-inventory.calculateTotalValue = function() {
-    return this.stockCount * this.price; // calculates total stock value
+    price: 200, // sets item price
+    calculateTotalValue: function() {
+        return this.stockCount * this.price; // calculates total stock value
+    }
 };
 console.log('Inventory Information:', inventory); // logs inventory details to console
 console.log('Total Inventory Value', inventory.calculateTotalValue()); // logs total value to console
