8cfb0a19f03f5cb7b1d2d57e5cc4b016ccc4d801
cjwattttts <cameron.jwatts@outlook.com>
2025-02-02 15:38:31 -0500

    Task 1 - Product Price Management

---
+++ cc_3.js
@@ -0,0 +1,6 @@
+//TASK 1
+let prices = [9.99, 14.99, 19.99, 24.99, 29.99] // declares array called prices
+prices.push(34.99); // adds new price to array
+prices.shift(); // removes first price from array
+console.log('Price List Updated') // updates price list in console
+
