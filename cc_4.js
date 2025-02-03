71a743f2b238cd66da424fb25752b3232f280c8b
cjwattttts <cameron.jwatts@outlook.com>
2025-02-02 22:52:22 -0500

    Task 2 - Sales Report

--- cc_4.js
+++ cc_4.js
@@ -4,3 +4,11 @@ if (purchaseAmount > 100) {
     finalAmount = purchaseAmount * 0.9; // applies a discount if amount is larger than 100
 }
 console.log('Final amount after discount: $${finalAmount}');
+
+// TASK 2
+let sales = [45, 300, 200, 350, 15]; // declares an array of sales figures
+let totalSales = 0; // sets the initial total sales
+for (let i = 0; i < sales.length; i++) {
+    totalSales += sales[i]; // adds each sale to the total sales
+}
+console.log ('Total sales: $${totalSales}');
