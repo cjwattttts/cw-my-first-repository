fbdb9ad82cf53a4cc6fc7fef133b3bba9fbbd861
cjwattttts <cameron.jwatts@outlook.com>
2025-02-02 22:47:14 -0500

    Task 1 - Customer Discounts.

---
+++ cc_4.js
@@ -0,0 +1,6 @@
+// TASK 1
+let purchaseAmount = 200 // declares purhcase amount
+if (purchaseAmount > 100) {
+    finalAmount = purchaseAmount * 0.9; // applies a discount if amount is larger than 100
+}
+console.log('Final amount after discount: $${finalAmount}');
